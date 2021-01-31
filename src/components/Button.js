9ff5d95f2945d339ace635d35a4b2078eb3f84c6
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-01-31 23:17:49 +0400

    update

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function Button() {
+function Button({ imp, text, link}) {
     return (
-        <div className="button">
-            Button
+        <div className={`button ${imp == 'secondary' ? 'button__white' : ''}`}>
+            <Link to={`$link`}>
+                {text}
+            </Link>
         </div>
     )
 }
